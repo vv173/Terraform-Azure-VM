429d7f20c9052fbc3b3c42a4a7d2aaa2fe523e90
Viktor Vodnev <v.vodnev73@gmail.com>
2022-04-18 21:52:44 +0200

    Add network security group and virtual network terraform objects

--- main.tf
+++ main.tf
@@ -2,3 +2,27 @@ resource "azurerm_resource_group" "default" {
   name     = "${var.prefix}-group"
   location = var.region
 }
+
+resource "azurerm_network_security_group" "default" {
+  name                = "${var.prefix}-security-group"
+  location            = azurerm_resource_group.default.location
+  resource_group_name = azurerm_resource_group.default.name
+}
+
+resource "azurerm_virtual_network" "default" {
+  name                = "${var.prefix}-network"
+  location            = azurerm_resource_group.default.location
+  resource_group_name = azurerm_resource_group.default.name
+  address_space       = ["10.10.0.0/16"]
+
+  subnet {
+    name           = "internal"
+    address_prefix = "10.10.2.0/24"
+    security_group = azurerm_network_security_group.default.id
+  }
+
+  # Tags should be added in the future.
+  # tags = {
+  #   environment = "Production"
+  # }
+}
